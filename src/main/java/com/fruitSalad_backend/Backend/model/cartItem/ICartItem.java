39a10c1e65766d38af131e45a6181696e640e249
willkupec <willem.kupec@gmail.com>
2023-08-14 18:03:15 +0200

    [FS-35] updated CartItem schema

--- src/main/java/com/fruitSalad_backend/Backend/model/cartItem/ICartItem.java
+++ src/main/java/com/fruitSalad_backend/Backend/model/cartItem/ICartItem.java
@@ -3,12 +3,10 @@ package com.fruitSalad_backend.Backend.model.cartItem;
 public interface ICartItem {
     public int getId();
     public void setId(int id);
-    public String getName();
-    public void setName(String name);
+    public String getTitle();
+    public void setTitle(String title);
     public double getPrice();
     public void setPrice(double price);
-    public String getSize();
-    public void setSize(String size);
-    public int getQuantity();
-    public void setQuantity(int quantity);
+    public String getSrc();
+    public void setSrc(String src);
 }
